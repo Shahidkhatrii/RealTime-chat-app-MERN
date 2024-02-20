2e187316c8c7240d00038757d2a28a21832b8ecc
shahidkhatrii <shahidkhatrixyz@gmail.com>
2024-02-20 16:38:22 +0530

    fixed typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import MainContainer from "./Components/MainContainer";
 import "./Styles/App.css";
-import Welcome from "./Components/Welcome";
 import ChatArea from "./Components/ChatArea";
 import Users from "./Components/Users";
 import Groups from "./Components/Groups";
 import CreateGroup from "./Components/CreateGroup";
 import { useSelector } from "react-redux";
-import MobileChats from "./Components/mobileChats";
+import MobileChats from "./Components/MobileChats";
 
 function App() {
   const lightTheme = useSelector((state) => state.themeKey);
